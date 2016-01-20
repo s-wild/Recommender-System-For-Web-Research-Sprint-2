96a61ef468a60b11f675c765723edd50e63345f2
upsd <Sam@Sams-MacBook-Pro.local>
2016-01-20 09:42:09 +0000

    Routes added for viewing .json data.
    Added a route to enable client to navigate to “/api/:file”, where
    “:file” is a string for which file the user wishes to view.
    
    e.g. “restaurants”, “activities”, “transport”

---
+++ app.js
@@ -0,0 +1,48 @@
+// Dependencies
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+// Data files
+var restaurants = require('./data/restaurants.json');
+var activities = require('./data/activities.json');
+var transport = require('./data/transport.json');
+
+// Routes
+app.get('/api/:file', function(req, res) {
+	if (req.params.file == 'undefined') {
+		res.end("File not recognised.");
+		return;
+	}
+	var file = req.params.file;
+
+	switch(file) {
+		case 'restaurants':
+			res.end(JSON.stringify(restaurants));
+			return;
+		case 'activities':
+			res.end(JSON.stringify(activities));
+			return;
+		case 'transport':
+			res.end(JSON.stringify(transport));
+			return;
+		default:
+			res.end("File not recognised");
+			return;
+	}
+
+});
+
+
+
+
+// Start server
+var server = app.listen(3000, function() {
+	var host = server.address().address;
+	var port = server.address().port;
+
+	console.log("Recommender System API listening at http://%s:%s", host, port);
+});
+
+
