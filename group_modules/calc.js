ff32f7e91b9710f4cd704cf23e32ece834f2bb61
up637415 <up637415@myport.ac.uk>
2016-01-20 14:30:33 +0000

    Merge cheapest functions into one function.

--- group_modules/calc.js
+++ group_modules/calc.js
@@ -2,7 +2,6 @@ module.exports = {
 	cheapest : getCheapest
 };
 
-
 // Data files
 var restaurants = require('../data/restaurants.json');
 var activities = require('../data/activities.json');
@@ -11,18 +10,21 @@ var transport = require('../data/transport.json');
 // Other modules created by us
 var util = require('./util.js');
 
-
+// From URL, detect cheapest type request.
 function getCheapest(file, res) {
 
 	switch(file) {
 		case 'restaurants':
-			getCheapestRestaurant(res);
+			type = 'restaurants';
+			getCheapestType(res, type);
 			break;
 		case 'activities':
-			getCheapestActivity(res);
+			type = 'activities';
+			getCheapestType(res, type);
 			break;
 		case 'transport':
-			getCheapestTransport(res);
+			type = 'transport';
+			getCheapestType(res, type);
 			break;
 		default:
 			res.end("File not recognised");
@@ -31,22 +33,31 @@ function getCheapest(file, res) {
 
 }
 
-function getCheapestRestaurant(res) {
-	var r = util.getNestedObject(restaurants, "restaurants");
-	var cheapest = getCheapestItem(r);
-	res.end(JSON.stringify(cheapest));
-}
+// Function to get data from type.
+function getCheapestType(res, type) {
+	var cheapest = getCheapestItem(type);
+	switch(type) {
+		case 'restaurants':
+			var r = util.getNestedObject(restaurants, "restaurants");
+			var cheapestRestaurant = getCheapestItem(r);
+			console.log("Cheapest Restaurant", cheapestRestaurant);
+			res.end(JSON.stringify(cheapestRestaurant));
+			break;
+		case 'activities':
+			var a = util.getNestedObject(activities, "activities");
+			var cheapestActivity = getCheapestItem(a);
+			res.end(JSON.stringify(cheapestActivity));
+			break;
+		case 'transport':
+			var t = util.getNestedObject(transport, "transport");
+			var cheapestTransport = getCheapestItem(t);
+			res.end(JSON.stringify(cheapestTransport));
+			break;
+		default:
+			res.end("Cheapest type not recognised.");
+			return;
+	}
 
-function getCheapestActivity(res) {
-	var a = util.getNestedObject(activities, "activities");
-	var cheapest = getCheapestItem(a);
-	res.end(JSON.stringify(cheapest));
-}
-
-function getCheapestTransport(res) {
-	var r = util.getNestedObject(transport, "transport");
-	var cheapest = getCheapestItem(r);
-	res.end(JSON.stringify(cheapest));
 }
 
 // Generic function for getting cheapest item, based on "avg_cost" field
