bb8b50bf045441b874c27043cb80eaabf41f80a5
up637415 <up637415@myport.ac.uk>
2016-02-04 15:22:29 +0000

    Adjusted user history word cloud. Now only displays when user is changed, not every time 'Get Recommendations is clicked.

--- assets/js/communicator.js
+++ assets/js/communicator.js
@@ -2,12 +2,20 @@ var userInput = "#users";
 
 $(document).ready(function() {
 
+	//Get user history for selected user on page load.
+	getUserHistory();
+
 	// Hide word on load.
 	$("#wordCloudWrapper").hide();
-	$("sectorButtons").hide();
+	$("#sectorButtons").hide();
 
 	// Event listeners
 	$("#getRecommBtn").click(getRecommendations);
+
+	// If user name is changed, get user history
+	$("#users").on("change keyup paste", function(){
+		getUserHistory();
+	});
 
 	// jQuery smooth scroll.
 	$(function() {
@@ -26,8 +34,9 @@ $(document).ready(function() {
 	});
 });
 
-function getRecommendations() {
 
+
+function getRecommendations() {
 	var user = $("#users").val();
 	var bracket = user.indexOf("(");
 	var userId = user.substring(bracket+1, bracket+2);
@@ -41,78 +50,85 @@ function getRecommendations() {
     	//alert(data);
     	// Put recommendations on page
     	$("#recommendations").html(data);
+
+			// Show cloud and sector buttons when there are recommendations.
+			$("#wordCloudWrapper").show();
+			$("#sectorButtons").show();
+
+			// Adjust display to show word cloud.
+			$("#userSelect").removeClass("offset-l4");
+			$("#userSelect").addClass("offset-l2");
+
   	});
+}
 
-		// Send query to URL for wordcloud
-		$.ajax({
-	  		url: "http://localhost:3000/api/frequencyofkeywords/" + userId
-		})
-	  	.done(function( dataWords ) {
-				// Show cloud and sector buttons when there are recommendations.
-				$("#wordCloudWrapper").show();
-				$("sectorButtons").show();
+function getUserHistory() {
+	var user = $("#users").val();
+	var bracket = user.indexOf("(");
+	var userId = user.substring(bracket+1, bracket+2);
+	var location = $("#locations").val();
 
-				// Adjust display to show word cloud.
-				$("#userSelect").removeClass("offset-l4");
-				$("#userSelect").addClass("offset-l2");
+	// Send query to URL for wordcloud
+	$.ajax({
+			url: "http://localhost:3000/api/frequencyofkeywords/" + userId
+	})
+		.done(function( dataWords ) {
 
-				// Parse JSON, seperate by sector.
-				var dataWordParse = JSON.parse(dataWords);
-				var restaurantsData = dataWordParse.restaurants;
-				var activitiesData = dataWordParse.activities;
-				var transportData = dataWordParse.transport;
-				var restaurantsKeywords = [];
-				var activitiesKeywords = [];
-				var transportKeywords = [];
-				var allKeywords = [];
+			// Parse JSON, seperate by sector.
+			var dataWordParse = JSON.parse(dataWords);
+			var restaurantsData = dataWordParse.restaurants;
+			var activitiesData = dataWordParse.activities;
+			var transportData = dataWordParse.transport;
 
-				// Adjusts the font size of word cloud.
-				var fontCloudSize = 20;
+			// Create empty arrays to store values retrived.
+			var restaurantsKeywords = [];
+			var activitiesKeywords = [];
+			var transportKeywords = [];
+			var allKeywords = [];
 
-				// Check there is user history for activities, if so, add values to array.
-				if(transportData[0] !== undefined || transportData.length !== 0) {
-						console.log("transportData exist.");
-						Object.keys(transportData).forEach(function(key) {
-							var wordTransport = transportData[key].word;
-							var countTransport = transportData[key].count * fontCloudSize;
-							allKeywords.push([wordTransport, countTransport]);
-						});
-				}
-				else {
-				    // does exist
-						console.log("transportData does not exist.");
-				}
+			// Adjusts the font size of word cloud.
+			var fontCloudSize = 20;
 
-				// Check there is user history for activities, if so, add values to array.
-				if(restaurantsData[0] !== undefined || restaurantsData.length !== 0) {
-						console.log("restaurantsData exist.");
-						Object.keys(restaurantsData).forEach(function(key) {
-				  		var wordsRestaurant = restaurantsData[key].word;
-				      var countRestaurant = restaurantsData[key].count * fontCloudSize;
-				      allKeywords.push([wordsRestaurant, countRestaurant]);
-				  	});
-				}
-				else {
-				    // does exist
-						console.log("restaurantsData does not exist.");
-				}
+			// Check there is user history for activities, if so, add values to array.
+			if(transportData[0] !== undefined || transportData.length !== 0) {
+					console.log("transportData exist.");
+					Object.keys(transportData).forEach(function(key) {
+						var wordTransport = transportData[key].word;
+						var countTransport = transportData[key].count * fontCloudSize;
+						allKeywords.push([wordTransport, countTransport]);
+					});
+			}
+			else {
+					console.log("transportData does not exist.");
+			}
 
-				// Check there is user history for activities, if so, add values to array.
-				if(activitiesData[0] !== undefined || activitiesData.length !== 0) {
-						console.log("activitiesData exist.");
-						Object.keys(activitiesData).forEach(function(key) {
-							var wordsActivities = activitiesData[key].word;
-							var countActivities = activitiesData[key].count * fontCloudSize;
-							allKeywords.push([wordsActivities, countActivities]);
-						});
-				}
-				else {
-						// does exist
-						console.log("activitiesData does not exist.");
-				}
+			// Check there is user history for activities, if so, add values to array.
+			if(restaurantsData[0] !== undefined || restaurantsData.length !== 0) {
+					console.log("restaurantsData exist.");
+					Object.keys(restaurantsData).forEach(function(key) {
+						var wordsRestaurant = restaurantsData[key].word;
+						var countRestaurant = restaurantsData[key].count * fontCloudSize;
+						allKeywords.push([wordsRestaurant, countRestaurant]);
+					});
+			}
+			else {
+					console.log("restaurantsData does not exist.");
+			}
 
-				// Initialise word cloud and assign all keywords that exist.
-				WordCloud(document.getElementById('wordCloud'), { list: allKeywords } );
-	  	});
+			// Check there is user history for activities, if so, add values to array.
+			if(activitiesData[0] !== undefined || activitiesData.length !== 0) {
+					console.log("activitiesData exist.");
+					Object.keys(activitiesData).forEach(function(key) {
+						var wordsActivities = activitiesData[key].word;
+						var countActivities = activitiesData[key].count * fontCloudSize;
+						allKeywords.push([wordsActivities, countActivities]);
+					});
+			}
+			else {
+					console.log("activitiesData does not exist.");
+			}
 
+			// Initialise word cloud and assign all keywords that exist.
+			WordCloud(document.getElementById('wordCloud'), { list: allKeywords } );
+		});
 }
